f81450d83a9803f2c91326f839395b1be5661d3a
Avinash <avinash.mait@gmail.com>
2017-12-03 21:24:43 +0530

    Anagrams : 2 more functions

--- src/main/scala/forcomp/Anagrams.scala
+++ src/main/scala/forcomp/Anagrams.scala
@@ -54,10 +54,15 @@ object Anagrams {
     * List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
     *
     */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = dictionary.map(wordOccurrences(_)).map(a => (a, dictionary.filter(b => wordOccurrences(b) == a))).toMap
+//  {
+//    val x: List[Occurrences] = dictionary.map(wordOccurrences(_))
+//    val x1 = x.map(a => (a, dictionary.filter(b => wordOccurrences(b) == a)))
+//    val x2 = x1.toMap
+//  }
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences.get(wordOccurrences(word)).fold(List[Word]())(a => a)
 
   /** Returns the list of all subsets of the occurrence list.
     * This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
